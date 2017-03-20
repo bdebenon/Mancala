02bc2248768a0fc88039d9ed6947257732d49768
My Pham <mypham194@tamu.edu>
2017-03-19 22:50:27 -0500

    GUI - initial commit

---
+++ GameUI.java
@@ -0,0 +1,10 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class GameUI extends JPanel {
+	// all GUI codes should be inside this class
+}
